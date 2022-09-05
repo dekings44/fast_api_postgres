27fb8a0d2181266114d467cdb329bec19266ef15
Kingsley Ihemere <dekings44@gmail.com>
2022-09-05 23:33:01 +0100

    fast api

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel): #act as a serializer
+    id:int
+    name:str
+    description:str
+    price:int
+    on_offer:bool
+
+
+@app.get('/')
+def index():
+    return {'message': 'Hello WOrld'}
+
+
+@app.get('/greet/{name}')
+def greet_name(name:str):
+    return {'greeting': f'Hello {name}, how are you doing today?'}
+
+
+@app.put('/item/{item_id}')
+def update_item(item_id:int, item:Item):
+    return {'name': item.name,
+            'description': item.description,
+            'price':item.price,
+            'on_offer':item.on_offer
+            }
