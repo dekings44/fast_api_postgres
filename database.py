11d0bfbb556d9f9b2484d3d61b3572663f9ca6b9
Kingsley Ihemere <dekings44@gmail.com>
2022-09-06 16:34:03 +0100

    fast api mini project

--- database.py
+++ database.py
@@ -1,5 +1,15 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+pwd = os.environ['PASSWORD']
+user = os.environ['USERNAME1']
 
 
-engine = create_engine("postgresql://postgre")
+engine = create_engine("postgresql://user:pwd@localhost/bookdb", echo = True)
+
+Base=declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
